ecedc4a95cb21270e2eb35554959fb484e0fcd29
Brian Moore <brianofmoore@gmail.com>
2020-05-28 14:57:13 -0700

    Fresh walkthrough - Finished through Module 3

--- src/App.js
+++ src/App.js
@@ -1,84 +1,89 @@
+// npm run start \\ npm run build for production
 import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
-let switcher = true
 
 const App = props => {
     const [ personState, setPersonState ] = useState({
         persons: [
-            { name: 'Brian', age: 40 },
-            { name: 'Lily', age: 25 },
-            { name: 'Cindy', age: 27 }
-        ]
+            { name: 'Brian', age: 40, id: 'bid2'},
+            { name: 'Lily', age: 25, id: 'lid3' },
+            { name: 'Cindy', age: 27, id: 'cid4' }
+        ],
+        showPersons: false
     })
 
-    const switchNameHandler = () => {
-        if (switcher) {
-            switcher = false
-            setPersonState ({
-                persons: [
-                    { name: 'Brian', age: 40 },
-                    { name: 'Lily', age: 25 },
-                    { name: 'Cindy', age: 27 }
-                ]
-            })
-        } else {
-            switcher = true
-            setPersonState ({
-                persons: [
-                    { name: 'BrianOfMoore', age: 40 },
-                    { name: 'Lily Nanner', age: 25 },
-                    { name: 'Cindy Crawford', age: 27 }
-                ]
-            })
-        }
+    const switchNameHandler = (event, id) => {
+        const personIndex = personState.persons.findIndex(p => {
+            return p.id === id
+        })
+
+        const person = {...personState.persons[personIndex]}
+
+        console.log(event.target)
+        person.name = event.target.value
+
+        const people = [...personState.persons]
+        people[personIndex] = person
+
+        setPersonState ({ showPersons: true, persons: [...people] })
+    }
+
+
+    const togglePersonsHandler = () => {
+        const doesShow = personState.showPersons
+        setPersonState({
+            ...personState,
+            showPersons: !doesShow
+        })
+    }
+
+    const deletePersonHandler = (personIndex) => {
+        console.log('madeIt')
+        console.log(personIndex)
+        const persons = [...personState.persons]
+        persons.splice(personIndex, 1)
+        console.log(persons)
+
+        setPersonState({persons: persons, showPersons: true})
+    }
+
+    const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+    }
+
+    let people = null
+
+    if (personState.showPersons) {
+        people = (
+            <div>
+                {personState.persons.map((person, index) => {
+                    return <Person
+                        click={() => deletePersonHandler(index)}
+                        name={person.name}
+                        age={person.age} 
+                        key={person.id}
+                        changed={(event) => {switchNameHandler(event, person.id)}}/>
+                })}
+            </div>
+        )
     }
 
     return (
         <div className="App">
           <header className="App-header">
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <button onClick={switchNameHandler}>Full Names</button>
-              Why Don't You Learn React
-            <Person
-             name={personState.persons[0].name}
-             age={personState.persons[0].age}
-             click={switchNameHandler}
-             />
-             <Person
-             name={personState.persons[1].name}
-             age={personState.persons[1].age}
-             />
-             <Person
-             name={personState.persons[2].name}
-             age={personState.persons[2].age}
-             />
+            <button 
+                style={style}
+                onClick={togglePersonsHandler}>Show Names</button>
+              
+            {people}
           </header>
         </div>
     )
-}
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Why Don't You Learn React
-        </a>
-        <Person></Person>
-      </header>
-    </div>
-  );
-} */
+} 
 
 export default App;
